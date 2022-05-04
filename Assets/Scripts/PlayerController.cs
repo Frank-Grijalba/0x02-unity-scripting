fc662d4f935a37316e399ed9f3ed6c85de40d734
Frank-Grijalba <fjgrijalba@gmail.com>
2022-05-04 18:10:23 -0500

    Task 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody Player;
+    public float speed = 1000f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
+        {
+            Player.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
+        {
+            Player.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        {
+            Player.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
+        {
+            Player.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
     }
 }
